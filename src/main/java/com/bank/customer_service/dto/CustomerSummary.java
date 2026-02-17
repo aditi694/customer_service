7e82f7043f7522b79e3f0b64a22140fdda32c59d
dml-aditi <aditi.goel@digimantra.com>
2026-02-17 10:28:53 +0530

    update

--- src/main/java/com/bank/customer_service/dto/CustomerSummary.java
+++ src/main/java/com/bank/customer_service/dto/CustomerSummary.java
@@ -1,12 +1,16 @@
 package com.bank.customer_service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerSummary {
 
     private UUID customerId;
